f29e3114c50f93715ceda5dee09d336620a510bf
Kévin Morpain <kevmorpain@gmail.com>
2023-04-07 10:57:58 +0200

    add base url in config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: "/guide-jdr/",
   title: "Guide JDR",
   description: "Guide JDR en markdown",
   themeConfig: {
